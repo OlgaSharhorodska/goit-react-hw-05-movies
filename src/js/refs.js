07b78599cab765c1212dfdd36fe5d7b4584cc5bb
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-11-06 18:08:42 +0200

    add js

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export const refs = {
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  API_KEY: '9322e843d0da07a43a222238c5af1c78',
+  POSTER_URL: 'https://image.tmdb.org/t/p/w500/',
+};
+
+//      --url 'https://api.themoviedb.org/3/search/movie?query=batman&include_adult=false&language=en-US&page=1' \
